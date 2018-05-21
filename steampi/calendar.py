1232b11fa6fbd8a0198973b93665cd43b4c579dc
Wok <woctezuma@users.noreply.github.com>
2018-05-22 00:04:11 +0200

    Catch case where release_date_as_str is an empty text

--- steampi/calendar.py
+++ steampi/calendar.py
@@ -17,13 +17,17 @@ def get_release_date_as_str(app_id):
 def get_release_date_as_datetime(app_id):
     release_date_as_str = get_release_date_as_str(app_id)
 
-    try:
-        # Reference: https://stackoverflow.com/a/6557568/
-        release_date = datetime.datetime.strptime(release_date_as_str, '%d %b, %Y')
-    except ValueError:
-        release_date = datetime.datetime.strptime(release_date_as_str, '%b %d, %Y')
-    except TypeError:
+    if release_date_as_str is not None and release_date_as_str == '':
         release_date = None
+    else:
+
+        try:
+            # Reference: https://stackoverflow.com/a/6557568/
+            release_date = datetime.datetime.strptime(release_date_as_str, '%d %b, %Y')
+        except ValueError:
+            release_date = datetime.datetime.strptime(release_date_as_str, '%b %d, %Y')
+        except TypeError:
+            release_date = None
 
     return release_date
 
