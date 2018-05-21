1bc1dd5c6f391ca83084754e7e28bd67df2a813a
Wok <woctezuma@users.noreply.github.com>
2018-05-21 23:15:12 +0200

    Change variable name: input_text rather than input_game_name

--- tests/tests_text_distances.py
+++ tests/tests_text_distances.py
@@ -7,16 +7,22 @@ import steampi.text_distances
 
 class TestTextDistancesMethods(unittest.TestCase):
     def test_compute_all_game_name_distances(self):
-        input_game_name = 'Crash Bandicoot'
-        text_distances = steampi.text_distances.compute_all_game_name_distances(input_game_name)
+        input_text = 'Crash Bandicoot'
+        text_distances = steampi.text_distances.compute_all_game_name_distances(input_text)
+
         self.assertGreater(len(text_distances), 0)
 
     def test_find_most_similar_game_names(self):
-        input_game_name = 'Crash Bandicoot'
         steamspy_database = steamspypi.load()
-        sorted_app_ids, _ = steampi.text_distances.find_most_similar_game_names(input_game_name, steamspy_database)
-        for i in range(5):
-            print(steamspy_database[sorted_app_ids[i]])
+
+        input_text = 'Crash Bandicoot'
+        sorted_app_ids, _ = steampi.text_distances.find_most_similar_game_names(input_text, steamspy_database)
+
+        num_games_to_print = 5
+        for i in range(num_games_to_print):
+            similar_game_name = steamspy_database[sorted_app_ids[i]]
+            print(similar_game_name)
+
         self.assertGreater(len(sorted_app_ids), 0)
 
 
