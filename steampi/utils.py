e2ac7bb100aa747e9026f57b711a4e726a723f8e
Wok <woctezuma@users.noreply.github.com>
2020-01-07 21:47:34 +0100

    Add function to remove characters from string s with s.translate()
    Reference: https://stackoverflow.com/a/266162

--- steampi/utils.py
+++ steampi/utils.py
@@ -1,3 +1,5 @@
+import string
+
 import steamspypi
 
 
@@ -17,3 +19,14 @@ def build_lower_case_game_name_dictionary(steamspy_database=None):
         lower_case_game_name_dictionary[lower_case_text] = app_id
 
     return lower_case_game_name_dictionary
+
+
+def remove_characters_from_str(input_str, characters_to_remove=None):
+    # Reference: https://stackoverflow.com/a/266162
+
+    if characters_to_remove is None:
+        characters_to_remove = string.punctuation
+
+    edited_str = input_str.translate(str.maketrans('', '', characters_to_remove))
+
+    return edited_str
--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,3 +1,4 @@
+import string
 import unittest
 
 import steampi.utils
@@ -8,6 +9,15 @@ class TestUtilsMethods(unittest.TestCase):
         lower_case_game_name_dictionary = steampi.utils.build_lower_case_game_name_dictionary()
 
         self.assertGreater(len(lower_case_game_name_dictionary), 0)
+
+    def test_remove_characters_from_str(self):
+        input_str = 'Hello, World!'
+        characters_to_remove = string.punctuation
+
+        edited_str = steampi.utils.remove_characters_from_str(input_str=input_str,
+                                                              characters_to_remove=characters_to_remove)
+
+        self.assertEqual(edited_str, 'Hello World')
 
 
 if __name__ == '__main__':
