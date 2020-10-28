e4cc8541b2a2c86da2eb7f19f55419beb7d8acbf
Wok <woctezuma@users.noreply.github.com>
2020-10-28 14:46:22 +0100

    Standardize setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,33 @@
-from distutils.core import setup
-
-# noinspection PyUnresolvedReferences
 import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='steampi',
-    packages=['steampi'],
+    version='0.5.1',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='Steam API on PyPI',
+    keywords=['steam', 'steamspy', 'api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/steampi',
+    download_url='https://github.com/woctezuma/steampi/archive/0.5.1.tar.gz',
+    packages=setuptools.find_packages(),
     install_requires=[
         'python-Levenshtein',
         'requests',
         'steamspypi',
     ],
-    version='0.5.1',
-    description='Steam API on PyPI',
-    long_description='SteamPI: a simple API for Steam, written in Python 3.',
-    long_description_content_type='text/x-rst',
-    author='Wok',
-    author_email='wok@tuta.io',
-    url='https://github.com/woctezuma/steampi',
-    download_url='https://github.com/woctezuma/steampi/archive/0.5.1.tar.gz',
-    keywords=['steam', 'steamspy', 'api'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Games/Entertainment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3',
 )
